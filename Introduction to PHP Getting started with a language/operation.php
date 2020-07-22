658a36c405a82248f434802af8e746ea6c116f9b
Bicalheira <bicalheira@users.noreply.github.com>
2020-07-22 16:12:12 -0300

    Learning how to do math operations

---
+++ Introduction to PHP Getting started with a language/operation.php
@@ -0,0 +1,13 @@
+<?php
+
+$yearsOld = 23;
+$in10Years = $yearsOld + 10;
+$years10ago = $yearsOld - 10;
+$double = $yearsOld * 2;
+$half = $yearsOld / 2;
+$square = $yearsOld ** 2;
+
+echo $in10Years . "\n";
+echo $years10ago . "\n";
+echo $double . "\n";
+echo $half . "\n";
