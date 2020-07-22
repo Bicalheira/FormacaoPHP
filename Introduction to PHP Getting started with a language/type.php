d11f964df410d67a30fb3fef8247d5801b1223d3
Bicalheira <bicalheira@users.noreply.github.com>
2020-07-22 16:14:03 -0300

    Learning how php works with the variables types

---
+++ Introduction to PHP Getting started with a language/type.php
@@ -0,0 +1,13 @@
+<?php
+
+$yearsOld = 23;
+$money = 1000.50;
+$name = "Lucas";
+$true = true;
+$false = false;
+
+echo gettype($yearsOld) . "\n";
+echo gettype($money) . "\n";
+echo gettype($name) . "\n";
+echo gettype($true) . "\n";
+echo gettype($false) . "\n";
