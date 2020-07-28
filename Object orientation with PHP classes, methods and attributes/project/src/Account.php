32ce6de912839b0307a1e054ab67eaec612eaf98
Bicalheira <bicalheira@users.noreply.github.com>
2020-07-28 15:10:28 -0300

    Added the function to deposit and withdraw money to the bank account

--- Object orientation with PHP classes, methods and attributes/project/src/Account.php
+++ Object orientation with PHP classes, methods and attributes/project/src/Account.php
@@ -4,5 +4,24 @@ class Account
 {
   public string $ownerCPF;
   public string $ownerName;
-  public float $balance;
+  public float $balance = 0;
+
+  public function withdraw(float $withdrawValue)
+  {
+
+    if ($withdrawValue > $this->balance) {
+      echo "Balance unavailable!" . PHP_EOL;
+    } else {
+      $this->balance -= $withdrawValue;
+    }
+  }
+
+  public function deposit(float $depositValue)
+  {
+    if($depositValue > 0){
+      $this->balance += $depositValue;
+    } else {
+      echo "The deposit value need be plus than zero!";
+    }
+  }
 }
