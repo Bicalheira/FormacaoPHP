3061daa7a35bbabcc879f7637c28ffb15adf8ccc
Bicalheira <bicalheira@users.noreply.github.com>
2020-07-28 22:04:11 -0300

    Learning about encapsulation and how it work

--- Object orientation with PHP classes, methods and attributes/project/src/Account.php
+++ Object orientation with PHP classes, methods and attributes/project/src/Account.php
@@ -2,9 +2,9 @@
 
 class Account
 {
-  public string $ownerCPF;
-  public string $ownerName;
-  public float $balance = 0;
+  private string $ownerCPF;
+  private string $ownerName;
+  private $balance = 0;
 
   public function withdraw(float $withdrawValue)
   {
@@ -35,6 +35,36 @@ class Account
     }
     if (!$this->withdraw($transferValue)) {
       $destinationAccount->deposit($transferValue);
-    } 
+    }
+  }
+
+  // public function setBalance(float $balance)
+  // {
+  //   $this->balance = $balance;
+  // }
+
+  public function getBalance(): float
+  {
+    return $this->balance;
+  }
+
+  public function getOwnerCPF(): string
+  {
+    return $this->ownerCPF;
+  }
+
+  public function setOwnerCPF(string $ownerCPF)
+  {
+    $this->ownerCPF = $ownerCPF;
+  }
+
+  public function getOwnerName(): string
+  {
+    return $this->ownerName;
+  }
+
+  public function setOwnerName(string $ownerName)
+  {
+    $this->ownerName = $ownerName;
   }
 }
