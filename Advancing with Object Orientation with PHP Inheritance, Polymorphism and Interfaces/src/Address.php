bc6f94c3dab6ca1e5a4750cf3cb7a8174cdbbb90
Bicalheira <bicalheira@users.noreply.github.com>
2020-07-30 09:52:51 -0300

    Added a new class called Address

---
+++ Advancing with Object Orientation with PHP Inheritance, Polymorphism and Interfaces/src/Address.php
@@ -0,0 +1,37 @@
+<?php
+
+class Address
+{
+  private string $city;
+  private string $street;
+  private string $number;
+  private string $zip;
+
+  public function __construct(string $city, string $street, string $number, string $zip)
+  {
+    $this->city = $city;
+    $this->street = $street;
+    $this->number = $number;
+    $this->zip = $zip;
+  }
+
+  public function getCity(): string
+  {
+    return $this->city;
+  }
+
+  public function getStreet(): string
+  {
+    return $this->street;
+  }
+
+  public function getNumber(): string
+  {
+    return $this->number;
+  }
+
+  public function getZip(): string
+  {
+    return $this->zip;
+  }
+}
