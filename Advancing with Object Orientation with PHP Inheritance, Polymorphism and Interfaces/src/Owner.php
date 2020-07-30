4bacbef0e2635b643a022d4dc14278d02f23154c
Bicalheira <bicalheira@users.noreply.github.com>
2020-07-30 10:45:36 -0300

    Added an extension of Person and cleaned the code

--- Advancing with Object Orientation with PHP Inheritance, Polymorphism and Interfaces/src/Owner.php
+++ Advancing with Object Orientation with PHP Inheritance, Polymorphism and Interfaces/src/Owner.php
@@ -1,35 +1,13 @@
 <?php
 
-class Owner
+class Owner extends Person
 {
-  private Cpf $cpf;
-  private string $name;
   private Address $address;
 
   public function __construct(Cpf $cpf, string $name, Address $address)
   {
-    $this->cpf = $cpf;
-    $this->validatesOwnerName($name);
-    $this->name = $name;
+    parent::__construct($name, $cpf);
     $this->address = $address;
-  }
-
-  public function getCpf()
-  {
-    return $this->cpf->getCpf();
-  }
-
-  public function getName()
-  {
-    return $this->name;
-  }
-
-  private function validatesOwnerName(string $ownerName)
-  {
-    if (strlen($ownerName) < 5) {
-      echo "The name needs at least five characters!";
-      exit();
-    }
   }
 
   public function getAddress(): Address
