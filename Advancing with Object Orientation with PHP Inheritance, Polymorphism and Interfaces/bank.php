648764e61a911bf578c99435ea3689ff9e6da7a8
Bicalheira <bicalheira@users.noreply.github.com>
2020-08-11 16:46:07 -0300

    New tests with two new account types, where both are different tariff value. Learning why we need to use abstract classes.

--- Advancing with Object Orientation with PHP Inheritance, Polymorphism and Interfaces/bank.php
+++ Advancing with Object Orientation with PHP Inheritance, Polymorphism and Interfaces/bank.php
@@ -1,21 +1,26 @@
 <?php
 
-require_once 'auto-load.php';
-
+use Alura\Bank\Model\Account\Account;
 use Alura\Bank\Model\Account\Owner;
+use Alura\Bank\Model\Account\SavingsAccount;
+use Alura\Bank\Model\Account\CurrentAccount;
 use Alura\Bank\Model\Address;
 use Alura\Bank\Model\Cpf;
-use Alura\Bank\Model\Account\Account;
 
-$account1 = new Account(new Owner(new Cpf('111.111.111-11'), 'Lucas', new Address('City', 'Street', '15', '2922929')));
+require_once 'src/auto-load.php';
+
+$account1 = new CurrentAccount(new Owner(new Cpf('111.111.111-11'), 'Lucas', new Address('City', 'Street', '15', '2922929')));
 
 $cpf = new Cpf('222.222.222-22');
 $address = new Address('City', 'Street', '115', '11111111');
 $bicalho = new Owner($cpf, 'Bicalho', $address);
-$account2 = new Account($bicalho);
+$account2 = new SavingsAccount($bicalho);
 
 $account1->deposit(500);
 $account2->deposit(500);
+
+$account1->withdraw(100);
+$account2->withdraw(100);
 
 echo $account1->getOwner() . PHP_EOL;
 echo $account1->getOwnerCPF() . PHP_EOL;
@@ -27,4 +32,4 @@ echo $account2->getOwnerCPF() . PHP_EOL;
 echo $account2->getBalance() . PHP_EOL;
 // echo $account2->getAddress() . PHP_EOL;
 
-var_dump($account1, $account2);
+// var_dump($account1, $account2);
